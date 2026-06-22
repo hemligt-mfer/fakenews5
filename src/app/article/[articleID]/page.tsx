7a7e88293d3f438ea29aca41e6c72a910c46248f
Adam Lundvall <adam.lundvall@gmail.com>
2026-06-22 10:49:16 +0200

    fcef

--- src/app/article/[articleID]/page.tsx
+++ src/app/article/[articleID]/page.tsx
@@ -23,7 +23,6 @@ export default async function ArticlePage({ params }: { params: Promise<{ articl
 
     const userId = await getUserId();
     const article = await getArticle(articleID);
-    console.log(article);
 
     let hasPermission = false;
     const session = await auth.api.getSession({
