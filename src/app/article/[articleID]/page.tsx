a75f1944145b7b9682fdd851cfd3b222a1d81589
Tose2514 <tobias.sederstrom@gmail.com>
2026-06-22 10:44:15 +0200

    markdown and imports

--- src/app/article/[articleID]/page.tsx
+++ src/app/article/[articleID]/page.tsx
@@ -1,9 +1,9 @@
 import {
-    addView,
-    getArticle,
-    getUserReaction,
-    hasUserBookmarkedArticle,
-    hasUserViewedArticle,
+  addView,
+  getArticle,
+  getUserReaction,
+  hasUserBookmarkedArticle,
+  hasUserViewedArticle,
 } from "@/_actions/article-actions";
 import Link from "next/link";
 import Likes from "./_components/likes";
@@ -17,135 +17,151 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import ArticleDoesntExist from "./_components/article-doesnt-exists";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm"
+import remarkIns from "remark-ins"
 
-export default async function ArticlePage({ params }: { params: Promise<{ articleID: string }> }) {
-    const { articleID } = await params;
+export default async function ArticlePage({
+  params,
+}: {
+  params: Promise<{ articleID: string }>;
+}) {
+  const { articleID } = await params;
 
-    const userId = await getUserId();
-    const article = await getArticle(articleID);
-    console.log(article);
+  const userId = await getUserId();
+  const article = await getArticle(articleID);
+  console.log(article);
 
-    let hasPermission = false;
-    const session = await auth.api.getSession({
-        headers: await headers(),
+  let hasPermission = false;
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session) {
+    const checkPermission = await auth.api.userHasPermission({
+      body: {
+        userId: session?.user.id,
+        permissions: {
+          article: ["read", "like", "dislike", "comment"],
+        },
+      },
     });
-    if (session) {
-        const checkPermission = await auth.api.userHasPermission({
-            body: {
-                userId: session?.user.id,
-                permissions: {
-                    article: ["read", "like", "dislike", "comment"],
-                },
-            },
-        });
-        if (checkPermission.success === true) {
-            hasPermission = true;
-        }
+    if (checkPermission.success === true) {
+      hasPermission = true;
+    }
+  }
+
+  if (userId && article.success && article.data) {
+    // Check if the user has viewed the article and add a view if not
+    const res = await hasUserViewedArticle(article.data.id, userId);
+    if (res.success && !res.data) {
+      await addView(articleID, userId);
+    }
+    const views = article.data.views.length;
+
+    // Calculate the total reactions (upvotes/downvotes) to one score
+    const reactions = article.data.reactions;
+    let totalReactions = 0;
+    for (const r of reactions) {
+      totalReactions += r.val;
     }
 
-    if (userId && article.success && article.data) {
-        // Check if the user has viewed the article and add a view if not
-        const res = await hasUserViewedArticle(article.data.id, userId);
-        if (res.success && !res.data) {
-            await addView(articleID, userId);
-        }
-        const views = article.data.views.length;
-
-        // Calculate the total reactions (upvotes/downvotes) to one score
-        const reactions = article.data.reactions;
-        let totalReactions = 0;
-        for (const r of reactions) {
-            totalReactions += r.val;
-        }
-
-        // Figure out if the user has reacted to the article and in that case if it
-        // was an upvote or downvote, reaction.data will be 1 for upvote, otherwise -1.
-        let userReaction;
-        const reaction = await getUserReaction(articleID, userId);
-        if (reaction.success && reaction.data) {
-            userReaction = reaction.data;
-        }
-
-        // Find out wether the user has bookmarked the article in question
-        let bookmarked;
-        const bookmark = await hasUserBookmarkedArticle(articleID, userId);
-        if (bookmark.success && bookmark.data === true) {
-            bookmarked = true;
-        } else {
-            bookmarked = false;
-        }
-
-        return (
-            <div className="p-2">
-                {article.data.category.length > 0 &&
-                    article.data.category.map((c, i) => {
-                        if (i + 1 !== article.data.category.length)
-                            return (
-                                <Link key={i} href={`/category/${c.id}`}>
-                                    {c.name},{" "}
-                                </Link>
-                            );
-                        else
-                            return (
-                                <Link key={i} href={`/category/${c.id}`}>
-                                    {c.name}
-                                </Link>
-                            );
-                    })}
-                <h1 className="font-extrabold text-2xl text-center">
-                    {article.data.title}</h1>
-                <p className="text-lg font-semibold text-center">
-                    by{" "}
-                    {article.data.author.map((a, i) =>
-                        i + 1 !== article.data.author.length ? `${a.alias}, ` : `${a.alias}`,
-                    )}
-                </p>
-                <p className="mt-2 mb-4">{article.data.content}</p>
-                <div className="flex border-b-2 mt-2 pb-2 text-sm">
-                    <div className="flex border-r pr-2">
-                        <Views num={views} />
-                    </div>
-
-                    <div className="flex border-r pr-2">
-                        <Likes
-                            articleId={article.data.id}
-                            userId={userId}
-                            userReaction={userReaction?.val}
-                            num={totalReactions}
-                        />
-                    </div>
-                    <div className="flex border-r pl-2 pr-2">
-                        <Bookmark articleId={articleID} userId={userId} bookmarked={bookmarked} />
-                    </div>
-
-                    <div className="flex ml-auto">
-                        {article.data.location ? article.data.location + ", " : ""}
-                        {format(article.data.createdAt, "yyyy-MM-dd HH:mm")}
-                    </div>
-                </div>
-                <h1 className="font-extrabold text-2xl text-center my-2">Comments</h1>
-                <div className="border-b-2 md:w-3xl mx-auto">
-                    {article.data.comments ? (
-                        <CommentarySection
-                            comments={article.data.comments}
-                            articleId={article.data.id}
-                        />
-                    ) : (
-                        ""
-                    )}
-                </div>
-                {userId !== null ? (
-                    <div className="mt-4">
-                        <TopLevelCommentForm articleId={article.data.id} />
-                    </div>
-                ) : (
-                    <Link href="login">Log in to write a comment.</Link>
-                )}
-            </div>
-        );
-    } else if (article.success === false) {
-        return <ArticleDoesntExist />;
-    } else if (!userId || !hasPermission) {
-        redirect(`preview/${articleID}`);
+    // Figure out if the user has reacted to the article and in that case if it
+    // was an upvote or downvote, reaction.data will be 1 for upvote, otherwise -1.
+    let userReaction;
+    const reaction = await getUserReaction(articleID, userId);
+    if (reaction.success && reaction.data) {
+      userReaction = reaction.data;
     }
+
+    // Find out wether the user has bookmarked the article in question
+    let bookmarked;
+    const bookmark = await hasUserBookmarkedArticle(articleID, userId);
+    if (bookmark.success && bookmark.data === true) {
+      bookmarked = true;
+    } else {
+      bookmarked = false;
+    }
+
+    return (
+      <div className="p-2">
+        {article.data.category.length > 0 &&
+          article.data.category.map((c, i) => {
+            if (i + 1 !== article.data.category.length)
+              return (
+                <Link key={i} href={`/category/${c.id}`}>
+                  {c.name},{" "}
+                </Link>
+              );
+            else
+              return (
+                <Link key={i} href={`/category/${c.id}`}>
+                  {c.name}
+                </Link>
+              );
+          })}
+        <h1 className="font-extrabold text-2xl text-center">
+          {article.data.title}
+        </h1>
+        <p className="text-lg font-semibold text-center">
+          by{" "}
+          {article.data.author.map((a, i) =>
+            i + 1 !== article.data.author.length
+              ? `${a.alias}, `
+              : `${a.alias}`,
+          )}
+        </p>
+        <article className="mt-2 mb-4 prose">
+          <ReactMarkdown remarkPlugins={[remarkGfm, remarkIns]}>{article.data.content}</ReactMarkdown>
+        </article>
+        <div className="flex border-b-2 mt-2 pb-2 text-sm">
+          <div className="flex border-r pr-2">
+            <Views num={views} />
+          </div>
+
+          <div className="flex border-r pr-2">
+            <Likes
+              articleId={article.data.id}
+              userId={userId}
+              userReaction={userReaction?.val}
+              num={totalReactions}
+            />
+          </div>
+          <div className="flex border-r pl-2 pr-2">
+            <Bookmark
+              articleId={articleID}
+              userId={userId}
+              bookmarked={bookmarked}
+            />
+          </div>
+
+          <div className="flex ml-auto">
+            {article.data.location ? article.data.location + ", " : ""}
+            {format(article.data.createdAt, "yyyy-MM-dd HH:mm")}
+          </div>
+        </div>
+        <h1 className="font-extrabold text-2xl text-center my-2">Comments</h1>
+        <div className="border-b-2 md:w-3xl mx-auto">
+          {article.data.comments ? (
+            <CommentarySection
+              comments={article.data.comments}
+              articleId={article.data.id}
+            />
+          ) : (
+            ""
+          )}
+        </div>
+        {userId !== null ? (
+          <div className="mt-4">
+            <TopLevelCommentForm articleId={article.data.id} />
+          </div>
+        ) : (
+          <Link href="login">Log in to write a comment.</Link>
+        )}
+      </div>
+    );
+  } else if (article.success === false) {
+    return <ArticleDoesntExist />;
+  } else if (!userId || !hasPermission) {
+    redirect(`preview/${articleID}`);
+  }
 }
