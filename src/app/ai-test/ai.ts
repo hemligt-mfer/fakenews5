ba61aad7a31090cea32ec6f8d155ee0dd4f1030a
Tose2514 <tobias.sederstrom@gmail.com>
2026-07-02 10:35:29 +0200

    structured ai

--- src/app/ai-test/ai.ts
+++ src/app/ai-test/ai.ts
@@ -1,12 +1,50 @@
 "use server";
-import { generateText } from "ai";
+import { generateImage, generateText, Output } from "ai";
 import { google } from "@ai-sdk/google";
+import z from "zod";
 
-export async function generateResponse(prompt: string): Promise<string> {
-    const { text } = await generateText({
-        model: google("gemini-2.5-flash"),
-        prompt: `Du är en journalist för en nyhetstidning. Du skriver korta humoristiska satir artiklar på engelska om: ${prompt}`,
-        tools: { google_search: google.tools.googleSearch({}) },
-    });
-    return text;
+const RecipeSchema = z.object({
+  recipe: z.object({
+    name: z.string().describe("The name of the recipe"),
+    description: z.string().describe("Short description of the recipe"),
+    ingredients: z.array(
+      z.object({
+        name: z.string().describe("Name of the ingredient"),
+        amount: z.string().describe("Amount of ingredient"),
+        unit: z.string().describe("Name of the measurement unit, g, dc, KG.."),
+      }),
+    ),
+    steps: z.array(z.string()),
+  }),
+});
+
+export type Recipe = z.infer<typeof RecipeSchema>;
+export type RecipeWithImage = Recipe & {
+  image?: string;
+};
+
+export async function generateResponse(
+  prompt: string,
+): Promise<RecipeWithImage> {
+  const { output } = await generateText({
+    model: google("gemini-2.5-flash"),
+    output: Output.object({ schema: RecipeSchema }),
+    prompt,
+    // tools: { google_search: google.tools.googleSearch({}) },
+  });
+  const image = await generateRecipeImage(output.recipe.name);
+  const recipeWithImage: RecipeWithImage = {
+    recipe: output.recipe,
+    image,
+  };
+  return recipeWithImage;
+}
+
+export async function generateRecipeImage(prompt: string): Promise<string> {
+  const { image } = await generateImage({
+    model: google.image("gemini-2.5-flash-image"),
+    prompt: `Generate an image of this dish ${prompt}`,
+  });
+  //upload to s3
+  return image.base64;
 }
--- src/app/ai-test/page.tsx
+++ src/app/ai-test/page.tsx
@@ -1,31 +1,51 @@
 "use client";
 
 import { useState } from "react";
-import { generateResponse } from "./ai";
+import { generateResponse, Recipe, RecipeWithImage } from "./ai";
 import Markdown from "react-markdown";
 
 export default function Page() {
-    const [isLoading, setIsLoading] = useState(false);
-    const [prompt, setPrompt] = useState("");
-    const [result, setResult] = useState("");
-    return (
-        <div>
-            <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
-            <button
-                onClick={async () => {
-                    setIsLoading(true);
-                    setResult(await generateResponse(prompt));
-                    setIsLoading(false);
-                }}
-            >
-                Generate
-            </button>
-            {isLoading && <p>Loading ...</p>}
-            {result && (
-                <article className="prose">
-                    <Markdown>{result}</Markdown>
-                </article>
-            )}
+  const [isLoading, setIsLoading] = useState(false);
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState<RecipeWithImage | null>(null);
+  return (
+    <div>
+      <input
+        type="text"
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <button
+        onClick={async () => {
+          setIsLoading(true);
+          setResult(await generateResponse(prompt));
+          setIsLoading(false);
+        }}
+      >
+        Generate
+      </button>
+      {isLoading && <p>Loading ...</p>}
+      {result && (
+        <div className="prose">
+          <h2>{result.recipe.name}</h2>
+          <img src={result.image} alt="Recipe image" />
+          <p>{result.recipe.description}</p>
+          <h3>Ingredients:</h3>
+          <ul>
+            {result.recipe.ingredients.map((i, index) => (
+              <li key={index}>
+                {i.name} {i.amount} {i.unit}
+              </li>
+            ))}
+          </ul>
+          <h3>Steps:</h3>
+          <ul>
+            {result.recipe.steps.map((s, index) => (
+              <li key={index}>{s}</li>
+            ))}
+          </ul>
         </div>
-    );
+      )}
+    </div>
+  );
 }
